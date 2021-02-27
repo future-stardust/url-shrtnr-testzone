b82e9a21f1e37940941f2b1100351e9efa156a94
retro <finerlo19@gmail.com>
2021-02-27 12:11:54 +0200

    Test1

--- src/test/java/edu/kpi/testcourse/HelloTest.java
+++ src/test/java/edu/kpi/testcourse/HelloTest.java
@@ -7,13 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class HelloTest {
 
   @Test
-  void compare(int a, int b){
-  if (a > b){
-    System.out.println(a);
-  } else if (a < b){
-    System.out.println(b);
-  }
-  }
+  void r(){assertThat(1).isEqualTo(1);}
 
 
 }
